b7bbb84fbb80ec5a4d324837f63d8d8482b9e1d0
Hanga <kof5656@naver.com>
2016-11-27 20:04:53 +0900

    signout

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,10 @@ module.exports = function(app, passport) {
     failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   }));
+  
+  app.get('/signout', function(req, res) {
+    req.logout();
+    req.flash('success', '로그아웃 되었습니다.');
+    res.redirect('/');
+  });
 };
