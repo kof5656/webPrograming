685938c41a131689db653bdcd02c827b54427f94
Hanga <kof5656@naver.com>
2016-11-27 18:35:35 +0900

    sign-in and password encryption

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express'),
     User = require('../models/User');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
