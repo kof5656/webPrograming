d17aba40eed25a3243518f7d247f494e9edd5141
Hanga <kof5656@naver.com>
2016-12-12 18:09:29 +0900

    user edit fix

--- routes/users.js
+++ routes/users.js
@@ -102,10 +102,6 @@ router.put('/:id', function(req, res, next) {
       req.flash('danger', '존재하지 않는 사용자입니다.');
       return res.redirect('back');
     }
-    if (user.validatePassword(user.password)) {
-      req.flash('danger', '현재 비밀번호가 일치하지 않습니다.');
-      return res.redirect('back');
-    }
 
     user.name = req.body.name;
     user.email = req.body.email;
